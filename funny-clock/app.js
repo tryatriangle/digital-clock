f780859120f96289e66ff609a07e70ad4c9a6dfe
harry <denton.harry@gmail.com>
2020-04-02 11:24:21 +0100

    clock now has seperate functions for time, display and joke selection. Needs stop button, analoge display and perhaps a different digital display

--- funny-clock/app.js
+++ funny-clock/app.js
@@ -1,31 +1,4 @@
-function showtime(){
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-
-    if(h < 10) {
-        h = "0" + h;
-    }
-
-    if(m < 10) {
-        m = "0" + m;
-    }
-
-    if(s < 10) {
-        s = "0" + s;
-    }
-
-    var time = h + ":" + m + ":" + s;
-    document.getElementById("clock-face").innerText = time;
-    document.getElementById("clock-face").textContent = time;
-
-    setTimeout(showtime, 1000);
-}
-
-showtime(); 
-
-// FUNNY CLOCK SCRIPT BELOW THIS LINE 
+// FUNNY CLOCK SCRIPT
 
 var hours;
 var minutes;
@@ -43,11 +16,37 @@ setInterval(updateClock,1000);
 
 updateClock();
 
+// function digital display clock
+
+function digitalDisplay(){
+    var date=new Date();
+
+    if(hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if(minutes < 10) {
+        minutes = "0" + minutes;
+    }
+
+    if(seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    var time = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock-face").innerText = time;
+    document.getElementById("clock-face").textContent = time;
+
+    setTimeout(digitalDisplay, 1000);
+}
+
+digitalDisplay(); 
+
 // function jokeSelector chooses a joke from the joke array (chosen randomly from the number of jokes in the array) and checks whether it should send anything to the div .
 
 function jokeSelector(){
 
-    var jokes = ["Your mother is so thick she failed a blood test.","Your mother is so ugly when she tried to join an ugly contest, they said \"sorry, no professionals\".","Your mother so brainless that if she spoke her mind she would be speechless.","Your mother is so stupid she sits on the TV and watches the sofa","Did you hear the one about the child with AIDS? It never gets old.", "What do Princess Dianna and Pink Floyd have in common? Their last big hit was the wall.", "How do you get a nun pregnant? Dress her up like an altar boy.", "How many potatoes does it take to kill an Irishman? None.", "EASILY identify who\'s constantly stinking out the office toilets by swapping the air-freshener for an air-horn.", "GREGGS STAFF. Shouting \"Yes, Chef!\" while sliding the steak bakes in the oven will give the shop a professional kitchen feel."];
+    var jokes = ["Your mother is so thick she failed a blood test.","Your mother is so ugly when she tried to join an ugly contest, they said \"sorry, no professionals\".","Your mother so stupid that if she spoke her mind she would be speechless.","Your mother is so stupid she sits on the TV and watches the sofa","Did you hear the one about the child with AIDS? It never gets old.", "What do Princess Dianna and Pink Floyd have in common? Their last big hit was the wall.", "How do you get a nun pregnant? Dress her up like an altar boy.", "How many potatoes does it take to kill an Irishman? None.", "EASILY identify who\'s constantly stinking out the office toilets by swapping the air-freshener for an air-horn.", "GREGGS STAFF. Shouting \"Yes, Chef!\" while sliding the steak bakes in the oven will give the shop a professional kitchen feel."];
 
     var randomJoke = "\"" + jokes[Math.floor(Math.random()*jokes.length)] + "\"";
 
